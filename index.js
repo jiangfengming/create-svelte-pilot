b86dca7be6f6af86c97ec272b7876224bb8730f9
fenix.sol <jiangfengming87@gmail.com>
2023-10-12 19:06:30 +0800

    change to degit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 
 const { spawn } = require('child_process');
-spawn('npx', ['--yes', 'tiged', 'jiangfengming/svelte-pilot-template'], { stdio: 'inherit' });
+spawn('npx', ['--yes', 'degit', 'jiangfengming/svelte-pilot-template', process.argv[2]], { stdio: 'inherit' });
